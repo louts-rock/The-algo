80d6b969352acd7e858ad06adb49a5e8bf1dbe3c
louts <rocklouts@sina.com>
2020-08-18 21:22:52 +0800

    Initial Commit

---
+++ ds/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  ds
+//
+//  Created by Apple on 2020/8/18.
+//  Copyright © 2020 louts. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
