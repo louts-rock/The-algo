042ac217970a6f1aa647317cc9db304c38144f93
louts <rocklouts@sina.com>
2020-08-29 14:16:11 +0800

    deject linklist.c

--- ds/linklist.h
+++ ds/linklist.h
@@ -20,5 +20,56 @@ typedef struct list_head{
 #define list_for_each(pos,head) \
     for(pos = (head)->next;pos->next,pos != head;\
             pos= pos->next)
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+static inline void LIST_HEAD_INIT(struct list_head *list)
+{
+    list->next =list;
+    list->prev =list;
+}
+static inline void __list_add(list_node *new,list_node *prev,list_node *next)
+{
+    next->prev = new;
+    new->next = next;
+    new->prev = prev;
+    prev->next = new;
+}
+static inline void list_add(list_node *new,struct list_head *head)
+{
+    __list_add(new,head,head->next);
+}
+static inline void list_add_tail(list_node *new,struct list_head *head)
+{
+    __list_add(new,head->prev,head);
+}
+
+static inline bool __list_add_valid(list_node *new,list_node *prev,list_node *next)
+{
+    return true;
+}
+
+static inline void list_insert(list_node *pos,list_node *new)
+{
+    list_node *prev = pos->prev;
+    prev->next = new;
+    new->prev  = prev;
+    new->next  = pos;
+    pos->prev  = new;
+}
+static inline void list_delete(list_node *pos)
+{
+    list_node  *prev,*next;
+    if(pos->next == pos)
+        return;
+    prev = pos->prev;
+    next = pos->next;
+    prev->next =  next;
+    next->prev = prev;
+    pos->next = NULL;
+    pos->prev = NULL;
+    
+}
+
 
 #endif /* linklist_h */
